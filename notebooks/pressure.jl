ed23f74033e106845bd379fd36309ee3bb8a553b
= <pjabardo@ipt.br>
2022-11-01 11:36:54 -0300

    Added other figures to pressure.jl
    There appear to be some mistakes in the original report (phases are inverted in figures 19 and 20).

--- notebooks/pressure.jl
+++ notebooks/pressure.jl
@@ -313,7 +313,195 @@ md"""
 
 
 # ╔═╡ b49ab635-2b7d-48ad-adbe-4dc4fc36cc08
+fig16 = [("points/bergh65-fig16-R0_50mm-ampl.txt", "points/bergh65-fig16-R0_50mm-phase.txt"), ("points/bergh65-fig16-R0_75mm-ampl.txt", "points/bergh65-fig16-R0_75mm-phase.txt"), ("points/bergh65-fig16-R1_25mm-ampl.txt", "points/bergh65-fig16-R1_25mm-phase.txt")]
 
+# ╔═╡ c4544166-470f-4f57-96db-92124f060dce
+pbergh16 = loadpoints.(fig16,".")
+
+# ╔═╡ 11d06d01-73c7-4cbf-ab5e-52ad0ca33b77
+press_fig16 = let R1=0.75, R2=[0.5, 0.75, 1.25],
+			 L=[0.25, 0.75], V=[0.0,300e-9], 
+             σ=0.0, k=1.4, γ=1.4, Pa=101325.0, Ta=288.15, 
+			 Pr=0.707, μ=1.82e-5
+	D = [[R1 * 2e-3, r*2e-3] for r in R2]
+	[PressureLine(d, L, V, σ, k; Ta=Ta, gamma=γ, Pa=Pa, Pr=Pr, mu=μ) for d in D]
+end
+
+# ╔═╡ 27421c02-6c13-4914-8a3a-8660075c19ab
+let fig=Figure(), press=press_fig16
+	pL = [correctcoef.(pL, freq) for pL in press]
+	ax1 = Axis(fig[1,1], title="Bergh and Tijdeman (1965) figure 16", xlabel="Frequency (Hz)", 
+		ylabel="Amplitude")
+	ax2 = Axis(fig[1,2], xlabel="Frequency (Hz)", 
+		ylabel="Phase")
+	R2 = [0.5,0.75,1.25]
+	for i in 1:3
+		p = pL[i]
+		bA = pbergh16[i][1]
+		bϕ = pbergh16[i][2]
+		A = abs.(p)
+		ϕ = phaseangle(p) * 180/π
+		lines!(ax1, freq, A, label="R₂ = $(R2[i]) mm")
+		scatter!(ax1, bA[:,1], bA[:,2])
+		lines!(ax2, freq, ϕ)
+		scatter!(ax2, bϕ[:,1], bϕ[:,2])
+	end
+	axislegend(ax1)
+	fig
+end
+
+# ╔═╡ d79b4d2c-56ff-4139-b0c4-62243b31c273
+md"""
+## Influence of R₂ on the dynamic response of a double pressure measuring system. Different lengths
+"""
+
+# ╔═╡ 4a4f8b3b-0774-43c6-9988-21acadd20cc9
+fig18 = [("points/bergh65-fig18-R0_50mm-ampl.txt", "points/bergh65-fig18-R0_50mm-phase.txt"), ("points/bergh65-fig18-R0_75mm-ampl.txt", "points/bergh65-fig18-R0_75mm-phase.txt"), ("points/bergh65-fig18-R1_25mm-ampl.txt", "points/bergh65-fig18-R1_25mm-phase.txt")]
+
+# ╔═╡ c594519b-7e22-4a31-93f9-afb7d16002c0
+pbergh18 = loadpoints.(fig18,".")
+
+# ╔═╡ 0f9ef798-2be2-4caa-9dbe-d12be4481610
+press_fig18 = let R1=0.75, R2=[0.5, 0.75, 1.25],
+			 L=[0.75, 0.25], V=[0.0,300e-9], 
+             σ=0.0, k=1.4, γ=1.4, Pa=101325.0, Ta=288.15, 
+			 Pr=0.707, μ=1.82e-5
+	D = [[R1 * 2e-3, r*2e-3] for r in R2]
+	[PressureLine(d, L, V, σ, k; Ta=Ta, gamma=γ, Pa=Pa, Pr=Pr, mu=μ) for d in D]
+end
+
+# ╔═╡ a5445dee-a141-4e86-ac88-e423d4baf2a5
+let fig=Figure(), press=press_fig18
+	pL = [correctcoef.(pL, freq) for pL in press]
+	ax1 = Axis(fig[1,1], title="Bergh and Tijdeman (1965) figure 18", xlabel="Frequency (Hz)", 
+		ylabel="Amplitude")
+	ax2 = Axis(fig[1,2], xlabel="Frequency (Hz)", 
+		ylabel="Phase")
+	R2 = [0.5,0.75,1.25]
+	for i in 1:3
+		p = pL[i]
+		bA = pbergh18[i][1]
+		bϕ = pbergh18[i][2]
+		A = abs.(p)
+		ϕ = phaseangle(p) * 180/π
+		lines!(ax1, freq, A, label="R₂ = $(R2[i]) mm")
+		scatter!(ax1, bA[:,1], bA[:,2])
+		lines!(ax2, freq, ϕ)
+		scatter!(ax2, bϕ[:,1], bϕ[:,2])
+	end
+	axislegend(ax1)
+	fig
+end
+
+# ╔═╡ b03ae350-da7c-41ae-b9ab-d1b93715d072
+md"""
+## Several configurations - Figure 19
+"""
+
+# ╔═╡ e9f4e4dd-f727-4558-8757-7824eecd6b5f
+# Probably a mistake on the article inverted the phases of II and III
+fig19 = [("points/bergh65-fig20-I-ampl.txt", "points/bergh65-fig20-I-phase.txt"), 
+("points/bergh65-fig19-II-ampl.txt", "points/bergh65-fig19-III-phase.txt"), #
+("points/bergh65-fig19-III-ampl.txt", "points/bergh65-fig19-II-phase.txt"), 
+("points/bergh65-fig20-IV-ampl.txt", "points/bergh65-fig20-IV-phase.txt")]
+
+# ╔═╡ 3b869e92-a63c-4c33-8567-d48488176523
+pbergh19 = loadpoints.(fig19,".")
+
+# ╔═╡ 55286adf-c25a-435b-8177-2e937187f836
+press_fig19 = let ra=0.75, rb=0.5
+	La = 0.75
+	Lb = 0.25
+	Da = ra*2e-3
+	Db = rb*2e-3
+	V = [0.0, 300e-9]
+	k = [1.4,1.4]
+	σ = [0.0, 0.0]
+	
+	[PressureLine(Da, 1.0, 300e-9, 0.0,1.4),
+	 PressureLine([Da,Db], [La,Lb], V, σ, k),
+	 PressureLine([Db,Da], [Lb,La], V, σ, k),
+	 PressureLine(Db, 1.0, 300e-9, 0.0,1.4)]
+	
+end
+
+# ╔═╡ 94ce520b-dc35-4918-a98c-ff4171c31c83
+let fig=Figure(), press=press_fig19
+	pL = [correctcoef.(pL, freq) for pL in press]
+	ax1 = Axis(fig[1,1], title="Bergh and Tijdeman (1965) figure 19", xlabel="Frequency (Hz)", 
+		ylabel="Amplitude")
+	ax2 = Axis(fig[1,2], xlabel="Frequency (Hz)", 
+		ylabel="Phase")
+	config = ["I", "II", "III", "IV"]
+	for i in 1:4
+		p = pL[i]
+		bA = pbergh19[i][1]
+		bϕ = pbergh19[i][2]
+		A = abs.(p)
+		ϕ = phaseangle(p) * 180/π
+		lines!(ax1, freq, A, label="$(config[i])")
+		scatter!(ax1, bA[:,1], bA[:,2])
+		lines!(ax2, freq, ϕ)
+		scatter!(ax2, bϕ[:,1], bϕ[:,2])
+	end
+	axislegend(ax1)
+	fig
+end
+
+# ╔═╡ cf233fa0-a9a1-4042-bcda-94ca80aa131d
+md"""
+## Several configurations - Figure 20
+"""
+
+# ╔═╡ 712b0917-6bf2-4bb9-943f-15624c4dcb9e
+# Probably a mistake on the figure. Inverted the phases of II and III
+
+fig20 = [("points/bergh65-fig20-I-ampl.txt", "points/bergh65-fig20-I-phase.txt"), 
+("points/bergh65-fig20-II-ampl.txt", "points/bergh65-fig20-III-phase.txt"), 
+("points/bergh65-fig20-III-ampl.txt", "points/bergh65-fig20-II-phase.txt"), 
+("points/bergh65-fig20-IV-ampl.txt", "points/bergh65-fig20-IV-phase.txt")]
+
+# ╔═╡ 8fe831b2-6a26-4d85-a52e-49fa41cedc5f
+pbergh20 = loadpoints.(fig20,".")
+
+# ╔═╡ 01175395-2ef5-4ed8-be00-b35824f2b1bd
+press_fig20 = let ra=0.75, rb=0.5
+	Da = ra*2e-3
+	Db = rb*2e-3
+	L = [0.5,0.5]
+	V = [0.0, 300e-9]
+	k = [1.4,1.4]
+	σ = [0.0, 0.0]
+	
+	[PressureLine([Da,Da], L, V, σ, k),
+	 PressureLine([Da,Db], L, V, σ, k),
+	 PressureLine([Db,Da], L, V, σ, k),
+	 PressureLine([Db,Db], L, V, σ, k)]
+	
+end
+
+# ╔═╡ 65b1ab55-af35-4a62-a7d5-840801dec4e9
+let fig=Figure(), press=press_fig20
+	pL = [correctcoef.(pL, freq) for pL in press]
+	ax1 = Axis(fig[1,1], title="Bergh and Tijdeman (1965) figure 20", xlabel="Frequency (Hz)", 
+		ylabel="Amplitude")
+	ax2 = Axis(fig[1,2], xlabel="Frequency (Hz)", 
+		ylabel="Phase")
+	config = ["I", "II", "III", "IV"]
+	for i in 1:4
+		p = pL[i]
+		bA = pbergh20[i][1]
+		bϕ = pbergh20[i][2]
+		A = abs.(p)
+		ϕ = phaseangle(p) * 180/π
+		lines!(ax1, freq, A, label="$(config[i])")
+		scatter!(ax1, bA[:,1], bA[:,2])
+		lines!(ax2, freq, ϕ)
+		scatter!(ax2, bϕ[:,1], bϕ[:,2])
+	end
+	axislegend(ax1)
+	fig
+end
 
 # ╔═╡ Cell order:
 # ╠═95d2c466-5910-11ed-2815-0d5b5cb4134b
@@ -327,31 +515,49 @@ md"""
 # ╠═3062ca4c-c3f6-4a92-8162-c6b40c634a2d
 # ╠═4ce7dc80-b1d7-4e30-b434-5388836bdc4b
 # ╠═895014f8-a512-4028-be26-c2283f13ec5f
-# ╠═3a733a31-90ce-40e3-a497-a5b7aa5add84
+# ╟─3a733a31-90ce-40e3-a497-a5b7aa5add84
 # ╠═2a17be96-a5ac-4457-b631-35a61c0b583b
 # ╠═e3e0ea67-cb2f-4fdd-a111-fdcafdfe878a
 # ╠═ae71115a-567d-4300-a88c-5ea3f3004cf3
 # ╠═60098f06-055b-413d-9ff3-919497727050
-# ╠═445099a5-e96c-41ac-85b7-b29645222fd6
+# ╟─445099a5-e96c-41ac-85b7-b29645222fd6
 # ╠═2ea662c3-5eb3-4d29-8172-98df45721b47
 # ╠═493f38d9-e876-4436-9ded-4f3c4507a498
 # ╠═e2a208d8-c5c3-4ab7-bc83-061a06dc8b43
 # ╠═40eebd93-5097-4f6d-b984-d738e5240fd4
-# ╠═e881f434-9895-400e-bb49-29b689e37df3
+# ╟─e881f434-9895-400e-bb49-29b689e37df3
 # ╠═4509fed8-9166-46f6-92ff-9fbf1a3ce930
 # ╠═2fe51e05-cb45-4de6-b8fc-f4cd91071354
 # ╠═01dff97b-63ae-47f8-aff8-9791e8a0101f
 # ╠═82ee815a-a99a-4e90-b5cf-f3639335cc61
-# ╠═ee02e6a3-81c4-4824-9aab-0c2d9bc245da
+# ╟─ee02e6a3-81c4-4824-9aab-0c2d9bc245da
 # ╠═377438c3-dad2-406a-a624-e688ee39ab7c
 # ╠═8ad2f108-89c6-4632-8e17-760e3c9f72b2
 # ╠═a0b9a783-7497-47c6-a9e3-21eb2100cbb9
 # ╠═f397fcaa-abd9-4e06-8cc2-11e85f4052b4
-# ╠═18be244d-4e02-4a80-97cc-777c258a1b09
+# ╟─18be244d-4e02-4a80-97cc-777c258a1b09
 # ╠═1243d194-10e0-4162-916b-3a7c41d93bcc
 # ╠═87d3db82-29b6-4aef-9805-1a721d29723e
 # ╠═478e20fe-890d-4cc2-ae0f-4eb17ce9d775
 # ╠═a3259d86-dfd1-4863-95a6-8bb713827121
-# ╠═7abe11fe-660e-446b-a2d7-cffac6c279a7
+# ╟─7abe11fe-660e-446b-a2d7-cffac6c279a7
 # ╠═ddcf5078-9f71-4461-a3cd-0e725798b76c
 # ╠═b49ab635-2b7d-48ad-adbe-4dc4fc36cc08
+# ╠═c4544166-470f-4f57-96db-92124f060dce
+# ╠═11d06d01-73c7-4cbf-ab5e-52ad0ca33b77
+# ╟─27421c02-6c13-4914-8a3a-8660075c19ab
+# ╠═d79b4d2c-56ff-4139-b0c4-62243b31c273
+# ╠═4a4f8b3b-0774-43c6-9988-21acadd20cc9
+# ╠═c594519b-7e22-4a31-93f9-afb7d16002c0
+# ╠═0f9ef798-2be2-4caa-9dbe-d12be4481610
+# ╟─a5445dee-a141-4e86-ac88-e423d4baf2a5
+# ╠═b03ae350-da7c-41ae-b9ab-d1b93715d072
+# ╠═e9f4e4dd-f727-4558-8757-7824eecd6b5f
+# ╠═3b869e92-a63c-4c33-8567-d48488176523
+# ╠═55286adf-c25a-435b-8177-2e937187f836
+# ╟─94ce520b-dc35-4918-a98c-ff4171c31c83
+# ╠═cf233fa0-a9a1-4042-bcda-94ca80aa131d
+# ╠═712b0917-6bf2-4bb9-943f-15624c4dcb9e
+# ╠═8fe831b2-6a26-4d85-a52e-49fa41cedc5f
+# ╠═01175395-2ef5-4ed8-be00-b35824f2b1bd
+# ╟─65b1ab55-af35-4a62-a7d5-840801dec4e9
